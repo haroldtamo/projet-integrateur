e66e91b610b670dfb3aaefb605d6ce1c65d8fe97
Harold TAMO <tamoharold@gmail.com>
2026-03-01 17:21:31 -0500

    Update Logo.jsx
    Edit title

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -3,9 +3,9 @@ import styles from './Logo.module.css'
 export default function Logo() {
     return <div className={styles.logo}>
         <div className={styles.title}>
-            <div>Programmation</div>
-            <span>Internet</span>
+            <div>Projet</div>
+            <span>Intégrateur</span>
         </div>
-        <div className={styles.number}>2</div>
+        <div className={styles.number}></div>
     </div>
 }
