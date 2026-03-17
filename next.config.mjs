59fa04d2a21f768b717fcccc253bc267302e7829
Harold TAMO <tamoharold@gmail.com>
2026-03-17 17:51:45 -0400

    Fix images errors

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
     reactStrictMode: true,
     output: 'export',
     trailingSlash: true,
+    basePath: '/projet-integrateur',
+    assetPrefix: '/projet-integrateur/',
     images: {
         unoptimized: true
     }
